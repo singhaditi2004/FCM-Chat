9cdf501b9e743805a0c6cae15237dd453bc2123a
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-21 11:19:37 +0530

    Completed

--- app/src/main/java/com/example/fcmchat/FirebaseService.java
+++ app/src/main/java/com/example/fcmchat/FirebaseService.java
@@ -1,5 +1,11 @@
 package com.example.fcmchat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -12,14 +18,50 @@ public class FirebaseService extends FirebaseMessagingService {
     public void onMessageReceived(@NonNull RemoteMessage message) {
         super.onMessageReceived(message);
         if (message.getNotification() != null) {
-            pushNotif(message.getNotification().getTitle(), message.getNotification().getBody());
+            pushNotification(message.getNotification().getTitle(), message.getNotification().getBody());
         }
     }
 
-    private void pushNotif(String title, String message) {
 
+    private void pushNotification (String title, String Msg) {
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification;
+        final String CHANNEL_ID = "push_noti";
+        Intent iNotify = new Intent(this, MainActivity.class);
+        iNotify.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pi = PendingIntent.getActivity(this, 100, iNotify, PendingIntent.FLAG_MUTABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Custom Channel";
+            String description = "Channel for Push Notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            if (nm != null) {
+                nm.createNotificationChannel(channel);
+
+            }
+            notification = new Notification.Builder(this)
+                    .setSmallIcon(R.drawable.notif)
+                    .setContentIntent(pi)
+                    .setContentTitle(title)
+                    .setSubText(Msg)
+                    .setAutoCancel(true)
+                    .setChannelId(CHANNEL_ID)
+                    .build();
+
+        } else {
+            notification = new Notification.Builder(this)
+                    .setSmallIcon(R.drawable.notif)
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .setContentTitle(title)
+                    .setSubText(Msg)
+                    .build();
+        }
+        if (nm != null)
+            nm.notify(1, notification);
     }
-
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
@@ -27,3 +69,4 @@ public class FirebaseService extends FirebaseMessagingService {
     }
 
 }
+/*if I want to implement a chat feature in a certain app and implement a search functionality such that user can search from all the chats and find the chat of user by searching their name like whatsapp also using firebase for implementing chat functionality*/
--- app/src/main/java/com/example/fcmchat/MainActivity.java
+++ app/src/main/java/com/example/fcmchat/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.fcmchat;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(!task.isSuccessful()){
+                    Log.e("ONCOMPLETEFIREBASE","Error some");
+                    return;
+                }
+                String token=task.getResult();
+                Log.d("TOKEN","TOKEN is "+token); //eB85wHWQQw2qZEBswQaIqn:APA91bH51SFMYIKznFjipl6edXEoUvq2Oa8kXaqOuxVNgDj-Y_OsnD5Z_zzS83MaXHeYSzCCzS8NyKed66x_HR7oE86wd9-M4SyRBMoQLw2NrNtuJwoAlyLzjdxo26ZnNzTovk0JqCBf
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
